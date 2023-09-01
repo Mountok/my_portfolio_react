6d14bca85a63b979e5398f1b8ef1f8c030ad107c
mountok <themountok@gmail.com>
2023-09-01 16:21:33 +0300

    commit

--- src/components/Main/Work/Slider/SliderData.js
+++ src/components/Main/Work/Slider/SliderData.js
@@ -2,20 +2,20 @@ export const SliderData = [
     {
         image: 'public/images/mma_app.png',
         title: 'MMA',
-        github: 'null',
-        browser: 'null'
+        github: 'https://github.com/Mountok/mma_site',
+        browser: 'https://splendorous-basbousa-57ea9b.netlify.app/'
     },
     {
         image: 'public/images/netlix_app.png',
         title: 'Netflix',
-        github: 'null',
+        github: 'https://github.com/Mountok/netflix-site',
         browser: 'https://dashuev-netflix-site.netlify.app'
 
     },
     {
         image: 'public/images/dashuev-ecommerce.png',
         title: 'House Staff',
-        github: 'null',
+        github: 'https://github.com/Mountok/e-commerce-react',
         browser: 'https://dashuev-ecommerce-app.netlify.app/'
 
     },
