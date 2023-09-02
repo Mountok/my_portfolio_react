d4b0777a7f194a96b7e2401c2d6cec9a286e6c49
mountok <themountok@gmail.com>
2023-09-02 13:34:52 +0300

    fix import images

--- src/components/Main/Work/Slider/SliderData.js
+++ src/components/Main/Work/Slider/SliderData.js
@@ -1,19 +1,19 @@
 export const SliderData = [
     {
-        image: 'public/images/mma_app.png',
+        image: './images/mma_app.png',
         title: 'MMA',
         github: 'https://github.com/Mountok/mma_site',
         browser: 'https://splendorous-basbousa-57ea9b.netlify.app/'
     },
     {
-        image: 'public/images/netlix_app.png',
+        image: './images/netlix_app.png',
         title: 'Netflix',
         github: 'https://github.com/Mountok/netflix-site',
         browser: 'https://dashuev-netflix-site.netlify.app'
 
     },
     {
-        image: 'public/images/dashuev-ecommerce.png',
+        image: './images/dashuev-ecommerce.png',
         title: 'House Staff',
         github: 'https://github.com/Mountok/e-commerce-react',
         browser: 'https://dashuev-ecommerce-app.netlify.app/'
